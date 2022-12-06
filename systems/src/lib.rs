0c4a45f0e0967af6bfa67a6d80e6e50537976cbc
Sven Richter <sveri80@gmail.com>
2022-12-06 10:23:45 +0100

    showing some spheres

--- systems/src/lib.rs
+++ systems/src/lib.rs
@@ -1,70 +1,102 @@
 mod utilities;
 
-use bevy::{prelude::*, sprite::collide_aabb};
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, sprite::collide_aabb, render::render_resource::{Extent3d, TextureDimension, TextureFormat}};
 // use components::Name as CompName;
 use components::*;
 use rand::{thread_rng, Rng};
 
-use smooth_bevy_cameras::{
-    controllers::orbit::{OrbitCameraBundle, OrbitCameraController},
-    LookTransform, LookTransformBundle, Smoother,
-};
+use smooth_bevy_cameras::controllers::orbit::{OrbitCameraBundle, OrbitCameraController};
 
 pub fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // commands.spawn(Camera3dBundle {
-    //     transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //     ..default()
-    // }).insert(Name::new("Camera3d"));
-
-    let eye = Vec3::new(-2.0, 2.5, 5.0);
-    let target = Vec3::default();
-
     commands
         .spawn(Camera3dBundle {
-            // camera_render_graph: CameraRenderGraph::new(bevy_strolle::graph::NAME),
             transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         })
         .insert(OrbitCameraBundle::new(
-            {
-                let mut controller = OrbitCameraController::default();
-
-                controller.mouse_rotate_sensitivity = Vec2::ONE * 0.2;
-                controller.mouse_translate_sensitivity = Vec2::ONE * 0.5;
-
-                controller
+            OrbitCameraController {
+                mouse_rotate_sensitivity: Vec2::ONE,
+                mouse_translate_sensitivity: Vec2::ONE * 0.5,
+                ..default()
             },
             Vec3::new(-20.0, 10.0, 20.0),
             Vec3::ZERO,
+        ))
+        .insert(Name::new("Camera3d"));
+
+    let mut i = 0;
+
+    let debug_material = materials.add(StandardMaterial {
+        base_color_texture: Some(images.add(uv_debug_texture())),
+        ..default()
+    });
+
+    while i < 10 {
+        commands.spawn((
+            PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::UVSphere { radius: 2.0, sectors: 100, stacks: 100 })),
+                material: debug_material.clone(),
+                transform: Transform::from_xyz(i as f32 * 10.0, 0.5, 0.0).with_rotation(Quat::from_rotation_x(-PI / 4.)),
+                ..default()
+            },
+            NameComponent("Circle 2".to_string()),
         ));
+        i += 1;
+    }
 
-    // commands
-    //     .spawn(LookTransformBundle {
-    //         // transform: LookTransform::new(eye, target),
-    //         transform: LookTransform::new(eye, target),
-    //         smoother: Smoother::new(0.9), // Value between 0.0 and 1.0, higher is smoother.
-    //     })
-    //     .insert(Camera3dBundle::default());
+    // commands.spawn(PbrBundle {
+    //     mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+    //     material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+    //     ..default()
+    // });
 
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.9 })),
-        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    // commands.spawn((
+    //     PbrBundle {
+    //         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.9 })),
+    //         material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+    //         transform: Transform::from_xyz(0.0, 0.5, 0.0),
+    //         ..default()
+    //     },
+    //     NameComponent("Cube 2".to_string()),
+    // ));
 
     commands.spawn((Galaxy, NameComponent("GalaxyOne".to_string())));
     commands.spawn((Galaxy, NameComponent("GalaxyTwo".to_string())));
+}
+
+/// Creates a colorful test pattern
+fn uv_debug_texture() -> Image {
+    const TEXTURE_SIZE: usize = 8;
+
+    let mut palette: [u8; 32] = [
+        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
+        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
+    ];
+
+    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+    for y in 0..TEXTURE_SIZE {
+        let offset = TEXTURE_SIZE * y * 4;
+        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
+        palette.rotate_right(4);
+    }
+
+    Image::new_fill(
+        Extent3d {
+            width: TEXTURE_SIZE as u32,
+            height: TEXTURE_SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+    )
 }
 
 #[no_mangle]
